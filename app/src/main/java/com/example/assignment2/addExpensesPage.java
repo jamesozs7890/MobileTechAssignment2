bedefce92dcce741cd3a50e55c8f05545982ce7c
Kurodo101 <73678149+Kurodo101@users.noreply.github.com>
2020-11-14 17:34:28 +0800

    Add Date Picker Code

--- app/src/main/java/com/example/assignment2/addExpensesPage.java
+++ app/src/main/java/com/example/assignment2/addExpensesPage.java
@@ -2,20 +2,27 @@ package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class addExpensesPage extends AppCompatActivity {
-
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expenses_page);
-
-        final EditText dateEditText = findViewById(R.id.editTextDate);
+        final TextView dateEditText = findViewById(R.id.editTextDate);
         final EditText amountEditText = findViewById(R.id.amountEditText);
         final EditText descriptionEditText = findViewById(R.id.extraDescPlainText);
         Button clearBtn = findViewById(R.id.clearButton);
@@ -35,6 +42,35 @@ public class addExpensesPage extends AppCompatActivity {
                 naviMain();
             }
         });
+        final DatePickerDialog.OnDateSetListener finalMDateSetListener = mDateSetListener;
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        addExpensesPage.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d("addExpensesPage", "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                dateEditText.setText(date);
+            }
+        };
     }
     public void naviMain(){
         Intent navi = new Intent(this,MainActivity.class);
