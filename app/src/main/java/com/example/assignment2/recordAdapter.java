aff9cc9d9679117ffe8ab6ac679262840bd95457
jamesozs7890 <61954268+jamesozs7890@users.noreply.github.com>
2020-11-30 21:23:51 +0800

    Correction

--- app/src/main/java/com/example/assignment2/recordAdapter.java
+++ app/src/main/java/com/example/assignment2/recordAdapter.java
@@ -46,7 +46,7 @@ public class recordAdapter extends BaseAdapter {
         Expenses e = this.getItem(position);
 
         date.setText(e.getExpenseDate());
-        amountVal.setText("RM" + e.getAmount().toString());
+        amountVal.setText(e.getAmount());
         categoryVal.setText(e.getCategory());
 
         return recordTile;
